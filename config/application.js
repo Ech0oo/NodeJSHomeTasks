6c5482be4d2e019b1fb011855949348c2ffc2c5e
Ech0oo <Ech0oo@tut.by>
2017-09-08 01:27:22 +0300

    import of the modules

--- config/application.js
+++ config/application.js
@@ -1,3 +1,4 @@
-export default Config {
+export default
+{
     "name": "NodeMP HW App"
 }
--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-import AppConfig from './config/application.json';
+import AppConfig from './config/application';
+import Product from './models/model/product';
+import User from './models/model/user';
 
-console.log(AppConfig);
+console.log(AppConfig.name);
+console.log(new Product);
+console.log(new User);
