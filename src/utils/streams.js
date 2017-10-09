052b7e2882b1614c19f90c2cdeaf0af0288bf287
Ech0oo <Ech0oo@tut.by>
2017-10-10 02:23:09 +0300

    add read stream and out file to console

--- src/utils/streams.js
+++ src/utils/streams.js
@@ -1,18 +1,19 @@
 const minimist = require('minimist');
+const fs = require('fs');
 const args = minimist(process.argv.slice(2));
 const WRONG_MESSAGE = "Wrong input!";
-const HELP_MESSAGE = "Help message.";
+const HELP_MESSAGE = "Please use command: node streams.js --action readFile --file ./../data/MOCK_DATA.csv";
 const methodObject = {
-    readFile: inputOutput(),
-    transformFile: transformFile(),
-    transform: transform(),
-    httpClient: httpClient(),
-    httpServer: httpServer(),
-    help: printHelpMessage()
+    readFile: inputOutput,
+    transformFile: transformFile,
+    transform: transform,
+    httpClient: httpClient,
+    httpServer: httpServer,
+    help: printHelpMessage
 }
 
 if (args.help) {
-    console.log(HELP_MESSAGE)
+    console.log(HELP_MESSAGE);
 }
 
 if (args.action && methodObject[args.action]) {
@@ -27,11 +28,16 @@ if (args.action && methodObject[args.action]) {
 
 
 function inputOutput(filePath) {
-    const readable = getReadableStreamSomehow();
-    const writable = fs.createWriteStream('file.txt');
+    const readable = fs.createReadStream(filePath);
+    readable.on('data', function(chunk) {
+        console.log(chunk);
+    });
+    readable.on('end', function() {
+        console.log("no more chunks");
+    });
+    // const writable = fs.createWriteStream('file.txt');
     // All the data from readable goes into 'file.txt'
-    readable.pipe(writable);
-    
+    // readable.pipe(transform);
 };
 function transformFile(filePath) { /* ... */ };
 function transform() { /* ... */ };
