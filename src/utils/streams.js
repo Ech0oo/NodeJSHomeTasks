8e74aa7646786b92c5c78659a11f278dc771fb30
Ech0oo <Ech0oo@tut.by>
2017-10-11 20:50:18 +0300

    draft commit

--- src/utils/streams.js
+++ src/utils/streams.js
@@ -1,5 +1,8 @@
 const minimist = require('minimist');
 const fs = require('fs');
+const through2 = require('through2');
+const csvjson = require('csvjson');
+
 const args = minimist(process.argv.slice(2));
 const WRONG_MESSAGE = "Wrong input!";
 const HELP_MESSAGE = "Please use command: node streams.js --action readFile --file ./../data/MOCK_DATA.csv";
@@ -14,6 +17,7 @@ const methodObject = {
 
 if (args.help) {
     console.log(HELP_MESSAGE);
+    return;
 }
 
 if (args.action && methodObject[args.action]) {
@@ -29,22 +33,43 @@ if (args.action && methodObject[args.action]) {
 
 function inputOutput(filePath) {
     const readable = fs.createReadStream(filePath);
-    readable.on('data', function(chunk) {
-        console.log(chunk);
-    });
-    readable.on('end', function() {
-        console.log("no more chunks");
+    readable.pipe(process.stdout);
+    readable.on('end', () => {
+        console.log("\nNo more chunks.");
     });
     // const writable = fs.createWriteStream('file.txt');
     // All the data from readable goes into 'file.txt'
     // readable.pipe(transform);
 };
-function transformFile(filePath) { /* ... */ };
-function transform() { /* ... */ };
+
+function transformFile(filePath) {
+    const readable = fs.createReadStream(filePath);
+    readable.pipe(through2(function (chunk, enc, callback) {
+        let chunk2 = new Buffer(chunk.toString().toUpperCase());
+        callback(null, chunk2);
+     }))
+    .pipe(process.stdout);
+
+    readable.on('finish', function () {
+      doSomethingSpecial()
+    })
+};
+
+/* from csv to json*/
+function transform() {
+    const readable = fs.createReadStream(filePath);
+    readable.pipe(through2(function (chunk, enc, callback) {
+        let chunk2 = chunk.toString();
+        const json = csvjson.toObject(chunk2);
+        callback(null, json);
+    }))
+    .pipe(process.stdout);
+};
+
 function httpClient() { /* ... */ };
 function httpServer() { /* ... */ };
 
-function printHelpMessage() { 
+function printHelpMessage() {
     console.log(HELP_MESSAGE);
 };
 
