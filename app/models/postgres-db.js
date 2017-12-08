e677515e402073356035e1ceaf6bfd3bb5ffafa5
Eduard Blazhevich <Ech0oo@tut.by>
2017-12-09 00:04:35 +0300

    code review changes

--- app/models/postgres-db.js
+++ app/models/postgres-db.js
@@ -5,35 +5,9 @@ const sequelize = new Sequelize(dbConfig.development.database, dbConfig.developm
     dialect: "postgres"
 });
 
-
-const Products = sequelize.define('Products',
-    {
-        name: {
-            type: Sequelize.STRING
-        },
-        modelId: {
-            type: Sequelize.INTEGER
-        }
-    });
-const Users = sequelize.define('Users',
-    {
-        name: {
-            type: Sequelize.STRING
-        },
-        password: {
-            type: Sequelize.STRING
-        },
-        email: {
-            type: Sequelize.STRING
-        }
-    });
-const Reviews = sequelize.define('Reviews',
-    {
-        review: {
-            type: Sequelize.TEXT
-        }
-    });
-console.log("MODELS!!!!!!!!!!!!!", sequelize.models);
+sequelize.import("./products");
+sequelize.import("./reviews");
+sequelize.import("./users");
 
 sequelize.sync();
 
