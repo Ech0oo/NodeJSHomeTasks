1b6ec9ebcdebb395c3cc26cc0a82999d7f12628a
Eduard Blazhevich <Ech0oo@tut.by>
2017-12-10 23:54:44 +0300

    fix deprecated settings for promises and mongo client

--- app/controllers/root-controller.js
+++ app/controllers/root-controller.js
@@ -1,7 +1,9 @@
 import Cities from "../models/cities-model";
 import mongoose from "mongoose";
 
-mongoose.connect('mongodb://localhost/task7');
+mongoose.Promise = global.Promise
+
+mongoose.connect('mongodb://localhost/task7', {useMongoClient: true});
 const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "connection error:"));
@@ -9,6 +11,16 @@ db.once("open", function() {
   console.log("Mongoose is connected!!!");
 });
 
+// export async function printRandomDocument(req, res) {
+//     try {
+//         const docArr = await Cities.find().exec();
+//         const max = docArr.length - 1;
+//         const randomNumber = Math.floor(Math.random() * (max + 1));
+//         res.json(docArr[randomNumber])
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+// }
 export async function printRandomDocument(req, res) {
     try {
         const docArr = await Cities.find().exec();
